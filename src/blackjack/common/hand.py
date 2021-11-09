8b1ed0492268587f7abfef8ae14e7bcd7a7f4e1c
ADAM MAGNESON <jamagneson@gmail.com>
2021-11-09 12:50:03 -0700

    Changed attribute name to cards

--- src/blackjack/common/hand.py
+++ src/blackjack/common/hand.py
@@ -4,23 +4,23 @@
 class Hand:
 
     def __init__(self, cards = []) -> None:
-        self.hand = cards
+        self.cards = cards
 
     def __add__(self, card: tuple):
-        self.hand.append(card)
+        self.cards.append(card)
 
     def split(self):
         """Split the hand"""
-        return Hand(cards=[self.hand.pop()])
+        return Hand(cards=[self.cards.pop()])
 
     def total(self):
         """Get the total value of the hand"""
         hand_value = 0
         aces = []
-        for card in self.hand:
+        for card in self.cards:
             card_value = 0
             if card[0] == 'A':
-                aces.append(self.hand.pop(card))
+                aces.append(self.cards.pop(card))
             elif card[0] in ['K', 'Q', 'J']:
                 card_value = 10
             else:
@@ -33,9 +33,9 @@ class Hand:
                 hand_value += 11
             else:
                 hand_value += 1
-            self.hand.pop(card)
+            self.cards.pop(card)
         return hand_value
     
     def reset(self):
         """Empty the hand at the end of a round"""
-        self.hand = []
+        self.cards = []
